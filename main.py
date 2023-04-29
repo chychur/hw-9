8bc0e459413da6b0100f54276bbcd9e8cdba99ff
Andrew <sideroalogo8@gmail.com>
2023-04-29 13:28:05 +0300

    had fixed *args

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 USERS = {}
 
-# decorator
+#decorator
 def input_error(func):
     def inner(*args):
         try:
@@ -14,29 +14,29 @@ def input_error(func):
             return 'Enter user name'
     return inner
 
-def unknown_command_handler(_):
+def unknown_command_handler():
     return "unknown_command"
 
-def hello_handler(_):
+def hello_handler():
     return "How can I help you?"
 
-def exit_handler(_):
+def exit_handler():
     return
 
 @input_error
-def add_handler(args):
-    name, phone = args
+def add_handler(name, phone):
+    
     USERS[name] = phone
     return f'User {name} added!'
 
 @input_error
-def change_handler(args): # change phone
-    name, phone = args
+def change_handler(name, phone): # change phone
+    
     old_phone = USERS[name]
     USERS[name] = phone
     return f'For user [ {name} ] had been changed phone number! \n Old phone number: {phone} \n New phone number: {old_phone}'
 
-def show_all_handler(_):
+def show_all_handler():
     result = ''
     header = '='*34 +'\n' + '|{:^4}|{:<12}|{:^14}|\n'.format('No.', 'Name', 'Phone') + '='*34 +'\n'
     foter = '='*34 +'\n' 
@@ -49,8 +49,8 @@ def show_all_handler(_):
     return result_tbl
 
 @input_error
-def phone_handler(args):
-    name = args
+def phone_handler(name, phone):
+    
     for name, pnone in USERS.items():
         return f'Phone {name} is: {pnone}\n'
 
@@ -76,15 +76,31 @@ def parse_input(user_input):
             command = command + ' ' + args[0]
             args = args[1:]
         handler = HANDLERS.get(command.lower(), unknown_command_handler)
-    return handler, args
+
+    try:
+        name = args[0]
+    except IndexError:
+        name = None
+
+    try:
+        phone = args[1]
+    except IndexError:
+        phone = None
+    return handler, name, phone
+    
+
 
 
 def main():
     while True:
         # example: add Petro 0991234567
         user_input = input('Please enter command and args: ')
-        handler, *args = parse_input(user_input)
-        result = handler(*args)
+        handler, name, phone = parse_input(user_input)
+        if name == None:
+            result = handler()
+        else:
+            result = handler(name, phone)
+        
         if not result:
             print('Good bye!')
             break
